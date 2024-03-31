4052da8c7fbbf5377a28b65ce831c6ed1d4b53ba
RowMax03 <51720224+RowMax03@users.noreply.github.com>
2024-03-31 20:47:50 +0200

    wp config php not being updated fixed

---
+++ project/srcs/requirements/wordpress/conf/create-config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Wait for MariaDB to start
+until mysql -h"mariadb" -u"$DB_USER" -p"$DB_PASSWORD" -e 'SELECT 1'; do
+	echo "Waiting for MariaDB to start"
+	sleep 1
+done
+
+## Create wp-config.php
+#wp config create --dbname="$DB_DATABASE" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="mariadb" --allow-root
+
+# Set WordPress environment variables
+wp config set USE_PERSISTENT_CONNECTION false --raw --type=constant --allow-root
+wp config set DB_NAME $DB_DATABASE --type=constant --allow-root
+wp config set DB_USER $DB_USER --type=constant --allow-root
+wp config set DB_PASSWORD $DB_PASSWORD --type=constant --allow-root
+wp config set DB_HOST mariadb --type=constant --allow-root
+#mv /var/www/html/wp-config.php /var/www/html/wordpress/wp-config.php
+
+exec "$@"
