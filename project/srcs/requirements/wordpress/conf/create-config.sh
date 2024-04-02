6f86e9a81ab9b0c77e823a7c20a832600723efe1
Max Reidenbach <mreidenb@student.42heilbronn.de>
2024-04-02 14:43:18 +0200

    trying wp setup in sh script instead of dockerfile

--- project/srcs/requirements/wordpress/conf/create-config.sh
+++ project/srcs/requirements/wordpress/conf/create-config.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 
+#check if wp-config.php exist
+if [ -f ./wp-config.php ]
+then
+	echo "wordpress already downloaded"
+else
+
+####### MANDATORY PART ##########
+
+	#Download wordpress and all config file
+	wget http://wordpress.org/latest.tar.gz
+	tar xfz latest.tar.gz
+	mv wordpress/* .
+	rm -rf latest.tar.gz
+	rm -rf wordpress
+
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
+	chmod +x wp-cli.phar && \
+	mv wp-cli.phar /usr/local/bin/wp
+
 # Wait for MariaDB to start
 until mysql -h"mariadb" -u"$DB_USER" -p"$DB_PASSWORD" -e 'SELECT 1'; do
 	echo "Waiting for MariaDB to start"
 	sleep 1
 done
+
+cd wordpress/
 
 ## Create wp-config.php
 wp core config --dbname="$DB_DATABASE" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="mariadb" --allow-root
