09bc3f3439627f8040608d7a165864ab9c80f222
Max Reidenbach <mreidenb@student.42heilbronn.de>
2024-04-03 14:37:50 +0200

    cleanup

--- project/srcs/requirements/wordpress/conf/create-config.sh
+++ project/srcs/requirements/wordpress/conf/create-config.sh
@@ -5,16 +5,14 @@ if [ -f ./wp-config.php ]
 then
 	echo "wordpress already downloaded"
 else
-
-####### MANDATORY PART ##########
-
-	#Download wordpress and all config file
+	# Download wordpress
 	wget http://wordpress.org/latest.tar.gz
 	tar xfz latest.tar.gz
 	mv wordpress/* .
 	rm -rf latest.tar.gz
 	rm -rf wordpress
 
+	# Install wp-cli
 	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && \
 	chmod +x wp-cli.phar && \
 	mv wp-cli.phar /usr/local/bin/wp
@@ -25,18 +23,16 @@ until mysql -h"mariadb" -u"$DB_USER" -p"$DB_PASSWORD" -e 'SELECT 1'; do
 	sleep 1
 done
 
-cd wordpress/
-
 ## Create wp-config.php
 wp core config --dbname="$DB_DATABASE" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="mariadb" --allow-root
 
-# Set WordPress environment variables
+# Set WordPress environment variables (redundant in case core config command fails)
 wp config set USE_PERSISTENT_CONNECTION false --raw --type=constant --allow-root
 wp config set DB_NAME $DB_DATABASE --type=constant --allow-root
 wp config set DB_USER $DB_USER --type=constant --allow-root
 wp config set DB_PASSWORD $DB_PASSWORD --type=constant --allow-root
 wp config set DB_HOST mariadb --type=constant --allow-root
-#mv /var/www/html/wp-config.php /var/www/html/wordpress/wp-config.php
+
 fi
 
 exec "$@"
